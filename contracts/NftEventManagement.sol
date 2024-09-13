33c0779693374a5ae080f5b0896e96a184e17b8a
Edoscoba <obisimon88@gmail.com>
2024-09-13 09:20:16 -0700

    nftEventManagement updated

--- contracts/NftEventManagement.sol
+++ contracts/NftEventManagement.sol
@@ -1,8 +1,7 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract EventSystem {
     address public nftContractAddress;
@@ -14,68 +13,67 @@ contract EventSystem {
         string description;
         uint256 duration;
         uint256 dateCreated;
-        bool isRegistered;
+        bool isActive;
     }
 
-    struct Users {
+    struct User {
         string name;
         string email;
     }
 
-    // mapping (uint256 id => mapping (address => bool)) registeredUsers;
-    mapping (uint256 => Event) eventCreated;
-    mapping (address => Event[]) events;
-    mapping (address => bool) hasRegistered;
-    mapping (address => Users) users;
+    mapping(uint256 => Event) public eventCreated;
+    mapping(uint256 => address[]) public eventRegistrants;
+    mapping(address => bool) public hasRegisteredForEvent;
+    mapping(address => User) public users;
 
     event EventCreatedSuccessfully(uint256 indexed id, string indexed name);
-    event UserRegistrationSuccessful(string indexed name, string indexed email);
+    event UserRegistrationSuccessful(uint256 indexed eventId, string indexed name, string indexed email);
 
     constructor(address _nftContractAddress) {
         nftContractAddress = _nftContractAddress;
-    } 
+    }
 
     function createEvent(uint256 _duration, string memory _name, string memory _description) external {
         require(msg.sender != address(0), "Address zero detected");
 
-        IERC721 _nftContract = IERC721(nftContractAddress);
-        require(_nftContract.balanceOf(msg.sender) > 0, "You must own an NFT to proceed");
+        IERC721 nftContract = IERC721(nftContractAddress);
+        require(nftContract.balanceOf(msg.sender) > 0, "You must own an NFT to proceed");
 
-        uint256 _id = eventCount++;
-        uint256 date = block.timestamp;
+        uint256 eventId = eventCount++;
+        uint256 dateCreated = block.timestamp;
 
-        Event storage evnt = eventCreated[_id];
+        Event storage newEvent = eventCreated[eventId];
 
-        evnt.id = _id;
-        evnt.name = _name;
-        evnt.description = _description;
-        evnt.duration = _duration;
-        evnt.dateCreated = date;
+        newEvent.id = eventId;
+        newEvent.name = _name;
+        newEvent.description = _description;
+        newEvent.duration = _duration;
+        newEvent.dateCreated = dateCreated;
+        newEvent.isActive = true;
 
-        evntCount += 1;
-
-        events[msg.sender].push(evnt);
-
-        emit EventCreatedSuccessfully(_id, _name);
-
+        emit EventCreatedSuccessfully(eventId, _name);
     }
 
-    function registerForEvent(uint256 _id, string memory _name, string memory _email) external {
+    function registerForEvent(uint256 _eventId, string memory _name, string memory _email) external {
         require(msg.sender != address(0), "Address zero detected");
 
-        Event storage evnt = eventCreated[_id];
-        require(evnt.id != 0, "invalid event id");
-        require(!evnt.isRegistered, "already registered for event");
+        Event storage evnt = eventCreated[_eventId];
+        require(evnt.id == _eventId, "Invalid event ID");
+        require(evnt.isActive, "Event is not active");
+        require(block.timestamp < evnt.dateCreated + evnt.duration, "Event registration ended");
+        require(!hasRegisteredForEvent[msg.sender], "Already registered for event");
 
-        uint256 _duration = evnt.dateCreated + evnt.duration;
-        require(block.timestamp < _duration, "Event registration ended");
+        users[msg.sender] = User(_name, _email);
+        eventRegistrants[_eventId].push(msg.sender);
+        hasRegisteredForEvent[msg.sender] = true;
 
-        users[msg.sender] = Users( _name, _email);
-        hasRegistered[msg.sender] = true;
-        evnt.isRegistered = true;
-
-        emit UserRegistrationSuccessful(_name, _email);
+        emit UserRegistrationSuccessful(_eventId, _name, _email);
     }
 
-    
+    function deactivateEvent(uint256 _eventId) external {
+     
+        Event storage evnt = eventCreated[_eventId];
+        require(evnt.id == _eventId, "Invalid event ID");
+        evnt.isActive = false;
+    }
 }
